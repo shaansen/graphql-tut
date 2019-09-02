e3684db476f14ae88b28017a7a0285e1141056a5
shaansen <shantanu.sengupta2008@gmail.com>
2019-09-02 12:22:19 -0400

    Initial Express Server setup

---
+++ schema/schema.js
@@ -0,0 +1,44 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema } = graphql;
+const axios = require("axios");
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        id: {
+          type: GraphQLString
+        }
+      },
+      resolve(_, args) {
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(response => response.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
